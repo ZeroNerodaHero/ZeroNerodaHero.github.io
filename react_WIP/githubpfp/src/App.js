ec2d13264a482de27596820a0f1646198df329c5
billy <billyz404meter@gmail.com>
2025-02-25 14:54:12 -0800

    updated hope it works

--- react_WIP/githubpfp/src/App.js
+++ react_WIP/githubpfp/src/App.js
@@ -1,8 +1,11 @@
 import backgroundImage from './res/background.gif';
-import './App.css';
 import ComponentBox from './components/componentbox/componentbox';
-import ArticleSnip from './components/article_snip/article_snip';
+import {ArticleSnip, RenderXML }from './components/article_snip/article_snip';
 import { MathJax, MathJaxContext } from "better-react-mathjax";
+import './App.css';
+import EconNotes from './res/Notes/2025_2_20_Econ.xml'
+import Abstract from './res/Notes/2025_2_23_Abstract.xml'
+import ATMNotes from './res/Notes/2025_2_24_ATM.xml'
 
 function App() {
   const style = {
@@ -17,14 +20,15 @@ function App() {
             <div className="sub-text">This is where i write about things</div>
           </div>
         } />
-
-        <ComponentBox 
-          inside_text={<ArticleSnip title={"Hello first post"} time={"2-20-2025"} text={
+        <RenderXML file={ATMNotes}/>
+        <RenderXML file={Abstract}/>
+        <RenderXML file={EconNotes}/>
+        <ArticleSnip title={"Hello first post"} time={"2-20-2025"} text={
             <div>
               "Its lowkey my birthday!" 
               <MathJax>{"\\(e^{i\\theta}=cos(\\theta)+isin(\\theta)\\)"}</MathJax>
             </div>
-          }/>}
+          }
         />
       </div>
     </MathJaxContext>
--- react_WIP/githubpfp/src/components/article_snip/article_snip.js
+++ react_WIP/githubpfp/src/components/article_snip/article_snip.js
@@ -1,19 +1,53 @@
 import './article_snip.css';
+import ComponentBox from '../componentbox/componentbox';
+import React, { useEffect, useState } from 'react';
+import { MathJax } from "better-react-mathjax";
 
-export function RenderXML(){
+
+export function RenderXML({file}) {
+    const [xmlData, setXmlData] = useState(null);
+
+    useEffect(()=>{
+        if(file ){
+            fetch(file).then( r => r.text())
+            .then(
+                (xmltext) => {
+                    console.log(xmltext)
+                    const parser = new DOMParser();
+                    const xmlDoc = parser.parseFromString(xmltext, "text/xml");
+                    const title = xmlDoc.getElementsByTagName('title')[0]?.textContent || 'Default Title';
+                    const time = xmlDoc.getElementsByTagName('date')[0]?.textContent || 'Default Time';
+                    const text = xmlDoc.getElementsByTagName('text')[0]?.textContent || 'Default Text';
+
+                    setXmlData({ title, time, text });
+                }
+            )
+        }
+    },[file])
     
-
+    
+    return (
+        <div>
+            {xmlData && <ArticleSnip title={xmlData.title} time={xmlData.time} text={xmlData.text} />}
+        </div>
+    );
 }
 
-function ArticleSnip({title,time,text}){
+export function ArticleSnip({title,time,text}){
     return (
-        <div className="articlesnip">
-            <div className="sub-text"> Written on: {time} </div>
-            <div className="title"> {title} </div>
-            <div>
-                {text}
+        <ComponentBox 
+          inside_text={
+            <div className="articlesnip">
+                <div className="sub-text"> Written on: {time} </div>
+                <div className="title"> {title} </div>
+                <div>
+                    <MathJax>
+                        {text}
+                    </MathJax>
+                </div>
             </div>
-        </div>
+          }
+        />
     )
 }
 
